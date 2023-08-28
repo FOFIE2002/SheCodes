c5a4d83a0f5154a6ceff164042d17fbcd5317fca
FOFIE2002 <fofiefopaelisabeth@gmail.com>
2023-08-28 18:57:04 +0100

    js math

--- SheCodes plus/Week3/index.js
+++ SheCodes plus/Week3/index.js
@@ -16,3 +16,25 @@ console.log(attraction);
 
 let phrase = `I went to visit ${attraction} in ${city}, right next to my school. now, I'm getting ready for my trip to ${country} `;
 console.log(phrase);
+
+//Js Maths
+
+let price = 4.5;
+let roundedPrice = Math.round(price);
+console.log(roundedPrice);
+
+let figure = 4.5;
+let roundPrice = Math.ceil(figure);
+console.log(roundPrice);
+
+let digit = 4.5;
+let roundDigit = Math.floor(digit);
+console.log(roundDigit);
+
+let numArray = [4, 1, 3, 10];
+let minNum = Math.min(...numArray);
+console.log(minNum);
+
+let numberArray = [4, 1, 3, 10];
+let maxNumber = Math.max(...numberArray);
+console.log(maxNumber);
