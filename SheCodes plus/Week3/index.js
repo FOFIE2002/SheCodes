82634f7b2cb786497d4502dd7c81a517033cf926
FOFIE2002 <fofiefopaelisabeth@gmail.com>
2023-09-02 19:20:22 +0100

    js arrays and loops

--- SheCodes plus/Week3/index.js
+++ SheCodes plus/Week3/index.js
@@ -39,44 +39,45 @@ let numberArray = [4, 1, 3, 10];
 let maxNumber = Math.max(...numberArray);
 console.log(maxNumber);
 
-
 // JS Arrays and Loops
 
 // Challenge 1
 // Create an array weekDays with all days of the week, console log the array
 let weekDays = [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday"
-  ];
-  console.log(weekDays);
-  
-  // Challenge 2
-  // Log ‘Monday’ and ‘Sunday’
-  console.log(weekDays[0])
-  console.log(weekDays[6])
-  
-  // Challenge 3
-  // Change ‘Sunday’ by ‘Funday’
-  // Log the array
-  weekDays[6] = "Funday"
-  console.log(weekDays[6])
-  
-  // Challenge 4
-  // Remove Monday and Tuesday
-  // Log the array
-  weekDays.splice(0, 2) 
-  console.log(weekDays)
-  
-  // Challenge 5
-  // Log every day of the week this way:`Temperature on Monday is 18 degrees’
-  let temperatures = [18, 20, 22, 19, 17, 21, 23];
-  
-  for (let i = 0; i < weekDays.length; i++) {
-    console.log(`Temperature on ${weekDays[i]} is ${temperatures[i]} degrees.`);
-  }
-  
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+console.log(weekDays);
+
+// Challenge 2
+// Log ‘Monday’ and ‘Sunday’
+console.log(weekDays[0]);
+console.log(weekDays[6]);
+
+// Challenge 3
+// Change ‘Sunday’ by ‘Funday’
+// Log the array
+weekDays[6] = "Funday";
+console.log(weekDays[6]);
+
+// Challenge 4
+// Remove Monday and Tuesday
+// Log the array
+weekDays.splice(0, 2);
+console.log(weekDays);
+
+// Challenge 5
+// Log every day of the week this way:`Temperature on Monday is 18 degrees’
+let temperatures = [18, 20, 22, 19, 17, 21, 23];
+for (let i = 0; i < weekDays.length; i++) {
+  console.log(`Temperature on ${weekDays[i]} is ${temperatures[i]} degrees.`);
+}
+
+
+// JS Objects
+
